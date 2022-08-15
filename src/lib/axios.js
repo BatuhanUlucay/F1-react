2b11cd3a72003c13250a0f135ded39f3d3b4e2b9
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-08-15 16:23:55 +0300

    axios is installed.

---
+++ src/lib/axios.js
@@ -0,0 +1,23 @@
+import Axios from 'axios';
+import { API_URL } from '../config';
+
+function requestInterceptor(config) {
+  config.headers.Accept = 'application/json';
+  return config;
+}
+
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.request.use(requestInterceptor);
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+
+    return Promise.reject(error);
+  }
+);
