f2e84cd7e15479d954fb632075a66bd27287e82a
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-08-16 09:55:10 +0300

    axios instance is configured.

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
+export const API_HOST = process.env.REACT_APP_API_HOST;
 export const API_URL = process.env.REACT_APP_API_URL;
-export const API_KEY = process.env.REACT_APP_API_URL;
+export const API_KEY = process.env.REACT_APP_API_KEY;
--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,15 +1,16 @@
 import Axios from 'axios';
-import { API_URL } from '../config';
+import { API_URL, API_KEY, API_HOST } from '../config';
 
 function requestInterceptor(config) {
   config.headers.Accept = 'application/json';
   return config;
 }
 
-console.log("URLLL", API_URL);
+console.log('keyy', API_KEY);
 
 export const axios = Axios.create({
   baseURL: API_URL,
+  headers: { 'x-rapidapi-key': API_KEY, 'x-rapidapi-host': API_HOST },
 });
 
 axios.interceptors.request.use(requestInterceptor);
