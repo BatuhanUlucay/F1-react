3bc7f127263cdc2c05a9529766762822eb38c4ed
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-08-17 10:19:26 +0300

    memoized the landing component in order to avoid unnecessary re-renders.

--- src/components/Layout/Landing.jsx
+++ src/components/Layout/Landing.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Timeline from '../../features/twitter/components/Timeline'
+import React, { useContext, memo } from 'react';
+import Timeline from '../../features/twitter/components/Timeline';
+import { useDriverRankings } from '../../features/rankings/api/getDriverRankings';
+import CurrentDriversContext from '../../context/CurrentDriversContext';
 
-function Landing() {
-  return (
-    <div>
-        {/* <Timeline /> */}
-    </div>
-  )
-}
+const Landing = memo(() => {
+  const { setDriverIds } = useContext(CurrentDriversContext);
 
-export default Landing
+  const driversQuery = useDriverRankings(new Date().getFullYear());
+
+  if (driversQuery.isLoading) {
+    return <h1>Loading..</h1>;
+  }
+  if (driversQuery.isSuccess) {
+    const currentSeasonDrivers = driversQuery.data.response;
+
+    let ids = [];
+
+    for (let i = 0; i < currentSeasonDrivers.length; i++) {
+      ids.push(currentSeasonDrivers[i].driver.id);
+    }
+
+    //setDriverIds(ids);
+
+    console.log(currentSeasonDrivers);
+
+    return <div>{/* <Timeline /> */}</div>;
+  }
+});
+
+export default Landing;
