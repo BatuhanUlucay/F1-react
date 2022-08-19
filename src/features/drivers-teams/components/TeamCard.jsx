a2675a16a96523649356ddc168010e65be5fa0a6
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-08-19 14:28:39 +0300

    displaying team logos now.

--- src/features/drivers-teams/components/TeamCard.jsx
+++ src/features/drivers-teams/components/TeamCard.jsx
@@ -5,6 +5,8 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import ImageIcon from '@mui/icons-material/Image';
+// import AspectRatio from '@mui/joy/AspectRatio';
 import { useTeam } from '../api/getTeam';
 import { testData } from './teamTestData';
 import wtf from 'wtf_wikipedia';
@@ -12,33 +14,29 @@ import { useTeamInfobox } from '../api/getTeamInfobox';
 import { hashMessage } from '../../../utils/md5hash';
 import { useTeamLogo } from '../api/getTeamLogo';
 
+import { checkUrlExists } from '../../../util/checkImageExists';
+
 function TeamCard({ team }) {
   function getImage(logoFileName) {
+    let url = '';
     if (logoFileName !== undefined) {
-      let url = '';
-
       const extracted = logoFileName.split(':').pop().replaceAll(' ', '_');
       const hash = hashMessage(extracted);
       const firstChar = hash.charAt(0);
       const firstAndSecond = firstChar + hash.charAt(1);
-      url = `https://upload.wikimedia.org/wikipedia/commons/${firstChar}/${firstAndSecond}/${extracted}`;
+      const urlFirst = `https://upload.wikimedia.org/wikipedia/commons/${firstChar}/${firstAndSecond}/${extracted}`;
 
-      var img = new Image();
-      img.src = url;
+      const urlSecond = `https://upload.wikimedia.org/wikipedia/en/${firstChar}/${firstAndSecond}/${extracted}`;
 
-      if (img.height !== 0) {
-        return url;
+      if (checkUrlExists(urlFirst)) {
+        url = urlFirst;
+      } else if (checkUrlExists(urlSecond)) {
+        url = urlSecond;
       } else {
-        url = `https://upload.wikimedia.org/wikipedia/en/${firstChar}/${firstAndSecond}/${extracted}`;
-        img.src = url;
-
-        if (img.height !== 0) {
-          return url;
-        } else {
-          return '';
-        }
+        url = '';
       }
     }
+    return url;
   }
 
   const wikiTitle = team.Constructor.url.split('/').pop();
@@ -50,31 +48,40 @@ function TeamCard({ team }) {
   if (infoboxQuery.isSuccess && teamLogoQuery.isSuccess) {
     let teamInfobox = infoboxQuery.data.infobox();
 
-    let urlLogo = ""
+    let urlLogo = '';
 
     if (teamInfobox.data.logo !== undefined) {
       const logoFileName = teamInfobox.data.logo.data.text;
-      
-      urlLogo = getImage(logoFileName)
+
+      urlLogo = getImage(logoFileName);
     }
 
     // const fileName = teamInfobox.data.logo.data.text.split(":").pop().replaceAll(" ", "_");
     // const hash1 = hashMessage(fileName);
     // const firstChar = hash1.charAt(0)
     // const firstAndSecond = firstChar + hash1.charAt(1);
+    {/* {(urlLogo !== 0) : <CardMedia
+      component="img"
+      alt="team logo"
+      //   height="140"
+      width="100"
+      image={urlLogo}
+    /> ? <div>
+    <ImageIcon fontSize="xl5" sx={{ color: 'text.tertiary' }} />
+  </div>} */}
 
     return (
       <Card className="max-w-xs mx-auto">
-        <CardMedia
-          component="img"
-          alt="team logo"
-          //   height="140"
-          width="100"
-          image={urlLogo}
-        />
         <CardContent>
+          {urlLogo !== "" ? <CardMedia
+      component="img"
+      alt="team logo"
+      height="140"
+      width="100"
+      image={urlLogo}
+    /> : <ImageIcon /> }
           <Typography variant="h5" component="div" className="m-auto">
-            {/* {team[0].name} */}
+            {team.Constructor.name}
           </Typography>
         </CardContent>
         {/* <CardActions>
