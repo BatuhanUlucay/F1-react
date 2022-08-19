5e465930ec139a5c289e789102cbc5bb9382a083
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-08-19 22:57:20 +0300

    bugfix about wiki images hotlinks.

--- src/features/drivers-teams/components/TeamCard.jsx
+++ src/features/drivers-teams/components/TeamCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -17,31 +17,7 @@ import { useTeamLogo } from '../api/getTeamLogo';
 import { doesImageExists } from '../../../util/checkImageExists';
 
 function TeamCard({ team }) {
-  
- async function getImage(logoFileName) {
-    const extracted = logoFileName.split(':').pop().replaceAll(' ', '_');
-    const hash = hashMessage(extracted);
-    const firstChar = hash.charAt(0);
-    const firstAndSecond = firstChar + hash.charAt(1);
-    const urlFirst = `https://upload.wikimedia.org/wikipedia/commons/${firstChar}/${firstAndSecond}/${extracted}`;
-
-    const urlSecond = `https://upload.wikimedia.org/wikipedia/en/${firstChar}/${firstAndSecond}/${extracted}`;
-
-    const doesImageExists1 = await doesImageExists(urlFirst);
-    const doesImageExists2 = await doesImageExists(urlSecond);
-
-    if (!!doesImageExists1) {
-      console.log("doesImage 1",doesImageExists1);
-      return urlFirst;
-    }
-
-    else if (!!doesImageExists2) {
-      return urlSecond;
-    }
-
-    return '';
-
-  }
+  const [imgSource, setImgSource] = useState('');
 
   const wikiTitle = team.Constructor.url.split('/').pop();
 
@@ -52,23 +28,51 @@ function TeamCard({ team }) {
   if (infoboxQuery.isSuccess && teamLogoQuery.isSuccess) {
     let teamInfobox = infoboxQuery.data.infobox();
 
-    let urlLogo = '';
-
     if (teamInfobox.data.logo !== undefined) {
       const logoFileName = teamInfobox.data.logo.data.text;
 
-      urlLogo = getImage(logoFileName);
-       console.log('asdasdasds', urlLogo);
+      const extracted = logoFileName.split(':').pop().replaceAll(' ', '_');
+      const hash = hashMessage(extracted);
+      const firstChar = hash.charAt(0);
+      const firstAndSecond = firstChar + hash.charAt(1);
+      const urlFirst = `https://upload.wikimedia.org/wikipedia/commons/${firstChar}/${firstAndSecond}/${extracted}`;
+
+      const urlSecond = `https://upload.wikimedia.org/wikipedia/en/${firstChar}/${firstAndSecond}/${extracted}`;
+
+      let img = new Image();
+      img.src = urlFirst;
+
+      img.onload = () => {
+        if (img.height !== 0) {
+          setImgSource(urlFirst);
+        }
+      };
+      let img2 = new Image();
+
+      img2.src = urlSecond;
+
+      img2.onload = () => {
+        if (img2.height !== 0) {
+          setImgSource(urlSecond);
+        }
+      };
     }
 
     return (
       <Card className="max-w-xs mx-auto">
         <CardContent>
-          {urlLogo !== "" ? (
-            <CardMedia component="img" alt="team logo" height="140" width="100" image={urlLogo} />
-          ) : (
-            <ImageIcon />
-          )}
+          <div className='h-28 w-28'>
+            {imgSource !== '' ? (
+              <CardMedia
+                component="img"
+                alt="team logo"
+                image={imgSource}
+                className="bg-cover"
+              />
+            ) : (
+              <ImageIcon />
+            )}
+          </div>
           <Typography variant="h5" component="div" className="m-auto">
             {team.Constructor.name}
           </Typography>
