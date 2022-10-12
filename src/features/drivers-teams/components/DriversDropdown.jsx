5618c36eddc8f1b386fdf8935dac0a621200e3c1
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-10-12 16:43:01 +0300

    Hover styling is added for dropdown elements.

--- src/features/drivers-teams/components/DriversDropdown.jsx
+++ src/features/drivers-teams/components/DriversDropdown.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDriverRankings } from '../../rankings/api/getDriverRankings';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 function DriversDropdown() {
+  const [hovered, setHovered] = useState('');
   const driverRankingsQuery = useDriverRankings(new Date().getFullYear());
+
+  const handleMouseOver = (e) => {
+    setHovered(e.target.innerText);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered('');
+  };
 
   if (driverRankingsQuery.isSuccess) {
     const drivers =
@@ -13,7 +22,15 @@ function DriversDropdown() {
         <div className="grid grid-rows-5 grid-flow-col justify-around">
           {drivers.map((driver) => {
             return (
-              <div className="text-white w-56 h-8 border-solid border-r border-b border-t-0 border-l-0 rounded-sm border-white mx-2 my-2 text-center gap-4">
+              <div
+                className={`w-56 h-8 border-solid border-r border-b border-t-0 border-l-0 rounded-sm ${
+                  driver.Driver.givenName + " " + driver.Driver.familyName === hovered
+                    ? 'border-red-600 text-red-600'
+                    : 'border-white text-white'
+                } mx-2 my-2 text-center gap-4`}
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+              >
                 <div>
                   {driver.Driver.givenName + ' ' + driver.Driver.familyName}{' '}
                   <KeyboardArrowRightIcon className="float-right" />
--- src/features/drivers-teams/components/TeamsDropdown.jsx
+++ src/features/drivers-teams/components/TeamsDropdown.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTeamRankings } from '../../rankings/api/getTeamRankings';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 function TeamsDropdown() {
+  const [hovered, setHovered] = useState('');
   const teamsQuery = useTeamRankings(new Date().getFullYear());
+
+  const handleMouseOver = (e) => {
+    setHovered(e.target.innerText);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered('');
+  };
+
+  //TODO: Take the colors from theme.
 
   if (teamsQuery.isSuccess) {
     const teams = teamsQuery.data.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
@@ -12,7 +23,15 @@ function TeamsDropdown() {
         <div className="grid grid-rows-3 grid-flow-col justify-around">
           {teams.map((team) => {
             return (
-              <div className="text-white h-8 w-56 border-solid border-r border-b border-t-0 border-l-0 rounded-sm border-white mx-8 my-4 text-center">
+              <div
+                className={`h-8 w-56 border-solid border-r border-b border-t-0 border-l-0 rounded-sm ${
+                  team.Constructor.name === hovered
+                    ? 'border-red-600 text-red-600'
+                    : 'border-white text-white'
+                } mx-8 my-4 text-center`}
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+              >
                 {team.Constructor.name}
                 <KeyboardArrowRightIcon className="float-right" />
               </div>
