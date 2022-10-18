42d55191031bca1753add2a416190662f067740b
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-10-18 16:03:00 +0300

    bugfix.

--- src/components/Layout/Landing.jsx
+++ src/components/Layout/Landing.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Timeline from '../../features/twitter/components/Timeline';
-import CurrentDriversContext from '../../context/CurrentDriversContext';
 import NextRace from '../../features/races/components/NextRace';
 
 const Landing = () => {
