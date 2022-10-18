c7dce955a3bcb9303475d7964d0a043e3ee1b9a8
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-10-18 15:09:04 +0300

    Link to detail page of drivers are added.

--- src/features/drivers-teams/components/DriverCard.jsx
+++ src/features/drivers-teams/components/DriverCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Typography } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import { useDriverPhoto } from '../api/getDriverPhoto';
+import { Link } from 'react-router-dom';
 
 function DriverCard({ driver }) {
   const [photo, setPhoto] = useState('');
@@ -23,20 +24,25 @@ function DriverCard({ driver }) {
     if (photo === '') setPhoto(profilePic.join('/'));
 
     return (
-      <div className="w-96 h-96 border-solid border-r border-t border-b-0 border-l-0">
-        <div className="flex w-full relative">
-          <Typography variant="h4">{driver.position}</Typography>
-          <Typography className="absolute right-1 my-2">{driver.points} points</Typography>
-        </div>
-        <Divider />
-        <div className="w-full h-auto grid grid-cols-2 my-8">
-          <img alt="Driver" src={photo} className="w-full h-full object-fit" />
-          <div>
-            <Typography>{`${driver.Driver.givenName} ${driver.Driver.familyName}`}</Typography>
-            <Typography>{`${driver.Constructors[0].name}`}</Typography>
+      <Link to={`/drivers/${driver.Driver.driverId}`} className="no-underline text-black">
+        <div className="w-96 h-96 border-solid border-r border-t border-b-0 border-l-0">
+          <div className="flex w-full relative">
+            <Typography variant="h4">{driver.position}</Typography>
+            <Typography className="absolute right-1 my-2">{driver.points} points</Typography>
+          </div>
+          <Divider />
+          <div className="w-full h-auto grid grid-cols-2 my-8">
+            <img alt="Driver" src={photo} className="w-full h-full object-fit" />
+            <div className="ml-6">
+              <Typography variant="h6">{`${driver.Driver.givenName} ${driver.Driver.familyName}`}</Typography>
+              <Typography
+                variant="h7"
+                className="text-sm"
+              >{`${driver.Constructors[0].name}`}</Typography>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
