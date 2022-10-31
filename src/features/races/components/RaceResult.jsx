0de4fd04ca2d910a50a73377c521dde5f12eaf85
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-10-31 11:41:02 +0300

    A timer icon is added to result page for fastest lap indicator.

--- src/features/races/components/RaceResult.jsx
+++ src/features/races/components/RaceResult.jsx
@@ -3,19 +3,20 @@ import { Link, useParams } from 'react-router-dom';
 import { useRaceResult } from '../api/getRaceResult';
 import GenericTable from '../../../components/Table/GenericTable';
 import { TableRow, TableCell } from '@mui/material';
+import TimerIcon from '@mui/icons-material/Timer';
 
 function RaceResult() {
   const { season, round } = useParams();
 
   const raceResultQuery = useRaceResult(season, round);
 
-  const columns = ['Position', 'Driver', 'Time', 'Status', 'Points'];
+  const columns = ['Position', 'Driver', 'Time', 'Status', 'Points', 'Fastest Lap'];
 
   if (raceResultQuery.isSuccess) {
     const raceResult = raceResultQuery.data.data.MRData.RaceTable.Races[0].Results;
 
-    const resultRows = raceResult.map((row) => (
-      <TableRow>
+    const resultRows = raceResult.map((row, index) => (
+      <TableRow key={index}>
         <TableCell />
         <TableCell>{row.position}</TableCell>
         <TableCell>
@@ -32,7 +33,9 @@ function RaceResult() {
         </TableCell>
         <TableCell>{row.status}</TableCell>
         <TableCell>{row.points}</TableCell>
-        <TableCell>{row.FastestLap?.rank === '1' ? 'F' : ''}</TableCell>
+        <TableCell>
+          {row.FastestLap?.rank === '1' ? <TimerIcon className="fill-purple-700" /> : ''}
+        </TableCell>
       </TableRow>
     ));
 
