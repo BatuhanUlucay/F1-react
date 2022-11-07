b752ab3d7f0f630e640b954906e0e02df8246560
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-07 14:52:49 +0300

    using https now.

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,9 @@
 import Axios from 'axios';
 
 export const ergastAxios = Axios.create({
-  baseURL: 'http://ergast.com/api/f1',
+  baseURL: 'https://ergast.com/api/f1',
 });
 
 export const wikiImageAxios = Axios.create({
-  baseURL: 'http://en.wikipedia.org/w/',
+  baseURL: 'https://en.wikipedia.org/w/',
 });
