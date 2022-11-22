ce79a20e4eb1abd990438dde845d478db96c315a
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-22 13:02:25 +0300

    Sprint race results are added.

--- src/features/races/components/RaceResult.jsx
+++ src/features/races/components/RaceResult.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useRaceResult } from '../api/getRaceResult';
 import { useQualiResult } from '../api/getQualifyingResult';
+import { useSprintResult } from '../api/getSprintResults';
 import GenericTable from '../../../components/Table/GenericTable';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -50,15 +51,18 @@ function RaceResult() {
 
   const raceResultQuery = useRaceResult(season, round);
   const qualiResultQuery = useQualiResult(season, round);
+  const sprintResultQuery = useSprintResult(season, round);
 
   const raceColumns = ['Position', 'Driver', 'Time', 'Status', 'Points', 'Fastest Lap'];
+  const sprintColumns = ['Grid Start', 'Position', 'Driver', 'Time', 'Status', 'Points'];
   const qualiColumns = ['Position', 'Driver', 'Time'];
 
-  if (raceResultQuery.isSuccess && qualiResultQuery.isSuccess) {
+  if (raceResultQuery.isSuccess && qualiResultQuery.isSuccess && sprintResultQuery.isSuccess) {
     const raceResult = raceResultQuery.data.data.MRData.RaceTable.Races[0]?.Results;
     const qualiResult = qualiResultQuery.data.data.MRData.RaceTable.Races[0]?.QualifyingResults;
+    const sprintResult = sprintResultQuery.data.data.MRData.RaceTable.Races[0]?.SprintResults;
 
-    console.log(qualiResult);
+    console.log(sprintResult);
 
     if (!search)
       setSearch(
@@ -67,6 +71,7 @@ function RaceResult() {
 
     let raceRows = undefined;
     let qualiRows = undefined;
+    let sprintRows = undefined;
 
     if (qualiResult) {
       qualiRows = qualiResult.map((row, index) => (
@@ -79,7 +84,32 @@ function RaceResult() {
             </Link>
             <div className="text-xs">{row.Constructor.name}</div>
           </TableCell>
-          <TableCell>{row.position <= 10 ? row.Q3 : row.position <= 15 ? 'Q2' : 'Q3'}</TableCell>
+          <TableCell>{row.position <= 10 ? row.Q3 : row.position <= 15 ? 'Q2' : 'Q1'}</TableCell>
+        </TableRow>
+      ));
+    }
+
+    if (sprintResult) {
+      sprintRows = sprintResult.map((row, index) => (
+        <TableRow key={index}>
+          <TableCell />
+          <TableCell>{row.grid}</TableCell>
+          <TableCell>{row.position}</TableCell>
+          <TableCell>
+            <Link to={`/drivers/${row.Driver.driverId}`} className="text-inherit">
+              {`${row.Driver.givenName} ${row.Driver.familyName}`}
+            </Link>
+            <div className="text-xs">{row.Constructor.name}</div>
+          </TableCell>
+          <TableCell>
+            {row.status === 'Finished' || row.status.includes('Lap')
+              ? row.Time?.time
+                ? row.Time.time
+                : 'No Time'
+              : 'DNF'}
+          </TableCell>
+          <TableCell>{row.status}</TableCell>
+          <TableCell>{row.points}</TableCell>
         </TableRow>
       ));
     }
@@ -105,7 +135,7 @@ function RaceResult() {
           <TableCell>{row.status}</TableCell>
           <TableCell>{row.points}</TableCell>
           <TableCell>
-            {row.FastestLap?.rank === '1' ? <TimerIcon className="fill-purple-700" /> : ''}
+            {row.FastestLap?.rank === '1' ? <TimerIcon className="fill-purple-700 ml-5" /> : ''}
           </TableCell>
         </TableRow>
       ));
@@ -133,22 +163,20 @@ function RaceResult() {
         ) : (
           ''
         )}
-        <Tabs value={value} onChange={handleChange} centered>
+        <Tabs value={value} onChange={handleChange} centered className="my-6">
           <Tab label="Qualifying" />
           <Tab label="Sprint" />
           <Tab label="Race" />
         </Tabs>
         {value === 0 && qualiResult ? (
           <GenericTable columns={qualiColumns} rows={qualiRows} />
+        ) : value === 1 && sprintResult ? (
+          <GenericTable columns={sprintColumns} rows={sprintRows} />
         ) : value === 2 && raceResult ? (
           <GenericTable columns={raceColumns} rows={raceRows} />
         ) : (
-          <h2 className='text-center'>No results.</h2>
+          <h2 className="text-center">No results.</h2>
         )}
-        {/* {value === 1 && sprintResult && <GenericTable columns={sprintColumns} rows={sprintRows} />} */}
-        {}
-
-        {/* <GenericTable columns={raceColumns} rows={resultRows} />; */}
       </div>
     );
   }
